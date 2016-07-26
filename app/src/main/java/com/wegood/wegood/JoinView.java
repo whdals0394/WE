ef247f72b47ae376d868ee00e61a3ed93597750c
whdals0394 <jongmin0394@naver.com>
2016-07-26 11:57:50 +0900

    two

---
+++ app/src/main/java/com/wegood/wegood/JoinView.java
@@ -0,0 +1,30 @@
+package com.wegood.wegood;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class JoinView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_view);
+    }
+    public void go(View v){
+        Intent it = getIntent();
+        String str_id= it.getStringExtra("it_id");
+        String str_pass= it.getStringExtra("it_pass");
+
+        Intent it_main = new Intent();
+        it_main.putExtra("it_id",str_id);
+        it_main.putExtra("it_pass",str_pass);
+        startActivity(it_main,Main.class);
+        finish();
+    }
+}
