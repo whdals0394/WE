5ff6fd0f37c28eb2d03f2b519d6937863ba45ff3
whdals0394 <jongmin0394@naver.com>
2016-07-27 11:30:21 +0900

    피니쉬~

--- app/src/main/java/com/wegood/wegood/DiaryWork.java
+++ app/src/main/java/com/wegood/wegood/DiaryWork.java
@@ -1,5 +1,6 @@
 package com.wegood.wegood;
 
+import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,6 +10,7 @@ import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,14 +28,44 @@ public class DiaryWork extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Date date = new Date();
+        date.setDate(date.getDate()+1);
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_diary_work);
-        TextView tv_day = (TextView)findViewById(R.id.day);
-        SimpleDateFormat simple = new SimpleDateFormat("yyyy년 MM월 dd일");
-        String str_day = simple.format(date);
+        setContentView(R.layout.activity_main);
+        TextView tv_day = (TextView) findViewById(R.id.day);
+        SimpleDateFormat CurYearFormat = new SimpleDateFormat("yyyy");
+        SimpleDateFormat CurMonthFormat = new SimpleDateFormat("MM");
+        SimpleDateFormat CurDayFormat = new SimpleDateFormat("dd");
+        String strCurYear = CurYearFormat.format(date);
+        String strCurMonth = CurMonthFormat.format(date);
+        String strCurDay = CurDayFormat.format(date);
+        String str_day = strCurYear + "년 " + strCurMonth + "월 " + strCurDay + "일 ";
         tv_day.append(str_day);
+        final int stryear = Integer.parseInt(strCurYear);
+        final int strmonth = Integer.parseInt(strCurMonth);
+        final int strday = Integer.parseInt(strCurDay);
+        findViewById(R.id.day).setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+
+                new DatePickerDialog(DiaryWork.this, dateSetListener, stryear, strmonth, strday).show();
+            }
+        });
+
 
     }
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            TextView tv_day = (TextView) findViewById(R.id.day);
+            String str_day = year + "년 " + monthOfYear + "월 " + dayOfMonth + "일 ";
+            tv_day.setText(null);
+            tv_day.append(str_day);
+        }
+
+    };
 
     public void ok(View v) {
         Intent it = getIntent();
@@ -41,12 +73,10 @@ public class DiaryWork extends AppCompatActivity {
         String str_pass = it.getStringExtra("it_pass");
         EditText et_title = (EditText) findViewById(R.id.title);
         String str_title = et_title.getText().toString();
-        TextView tv_day = (TextView)findViewById(R.id.day);
+        TextView tv_day = (TextView) findViewById(R.id.day);
         EditText et_letter = (EditText) findViewById(R.id.letter);
         String str_letter = et_letter.getText().toString();
-        SimpleDateFormat simple = new SimpleDateFormat("yyyy년 MM월 dd일");
-        String str_day = simple.format(date);
-        tv_day.append(str_day);
+        String str_day = tv_day.getText().toString();
         try {
             dbmanager = new DBmanager(this);
             sqlitedb = dbmanager.getWritableDatabase();
