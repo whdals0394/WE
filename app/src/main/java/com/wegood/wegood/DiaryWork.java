f45eb99ce32d4129ece4d8c23907b990d6ad5213
whdals0394 <jongmin0394@naver.com>
2016-07-27 10:23:59 +0900

    good

--- app/src/main/java/com/wegood/wegood/DiaryWork.java
+++ app/src/main/java/com/wegood/wegood/DiaryWork.java
@@ -4,35 +4,49 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class DiaryWork extends AppCompatActivity {
     SQLiteDatabase sqlitedb;
     DBmanager dbmanager;
     Date date = new Date();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_work);
+        TextView tv_day = (TextView)findViewById(R.id.day);
+        SimpleDateFormat simple = new SimpleDateFormat("yyyy년 MM월 dd일");
+        String str_day = simple.format(date);
+        tv_day.append(str_day);
+
     }
+
     public void ok(View v) {
         Intent it = getIntent();
         String str_id = it.getStringExtra("it_id");
         String str_pass = it.getStringExtra("it_pass");
-        EditText et_title = (EditText)findViewById(R.id.title);
+        EditText et_title = (EditText) findViewById(R.id.title);
         String str_title = et_title.getText().toString();
-
-        EditText et_letter = (EditText)findViewById(R.id.letter);
+        TextView tv_day = (TextView)findViewById(R.id.day);
+        EditText et_letter = (EditText) findViewById(R.id.letter);
         String str_letter = et_letter.getText().toString();
-        SimpleDateFormat simple = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat simple = new SimpleDateFormat("yyyy년 MM월 dd일");
         String str_day = simple.format(date);
+        tv_day.append(str_day);
         try {
             dbmanager = new DBmanager(this);
             sqlitedb = dbmanager.getWritableDatabase();
--- app/src/main/java/com/wegood/wegood/Login.java
+++ app/src/main/java/com/wegood/wegood/Login.java
@@ -1,10 +1,8 @@
 package com.wegood.wegood;
 
-import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,68 +10,78 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-    SQLiteDatabase sqlitedb;
     DBmanager dbmanager;
-
+    SQLiteDatabase sqlitedb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-
-    public void login(View v) {
-        EditText et_id = (EditText) findViewById(R.id.id);
-        EditText et_pass = (EditText) findViewById(R.id.pass);
+    public void login(View v){
+        EditText et_id = (EditText)findViewById(R.id.id);
+        EditText et_pass = (EditText)findViewById(R.id.pass);
         String str_id = et_id.getText().toString();
-        String str_pass = et_id.getText().toString();
-        if (str_id.equals("") || str_id == null) {
-            Toast.makeText(this, "아이디를 입력해주세요", Toast.LENGTH_LONG).show();
+        String str_pass = et_pass.getText().toString();
+        if(str_id.equals("")||str_id==null){
+            Toast.makeText(this,"아이디를 입력해주세요", Toast.LENGTH_LONG).show();
             return;
-        } else if (str_id.equals("") || str_id == null) {
-            Toast.makeText(this, "비밀번호를 입력해주세요", Toast.LENGTH_LONG).show();
+        } else if (str_pass.equals("") || str_pass == null) {
+            Toast.makeText(this,"비밀번호를 입력해주세요", Toast.LENGTH_LONG).show();
             return;
         }
-        try {
-            loginwork(str_id, str_pass);
-        } catch (Exception e) {
+        try{
+            loginwork(str_id,str_pass);
+        }catch (Exception e){
             e.getStackTrace();
         }
     }
-
-    public void loginwork(String id, String pass) {
+    public void loginwork(String id,String pass){
         dbmanager = new DBmanager(this);
         sqlitedb = dbmanager.getReadableDatabase();
-        String sql = "select * from Diary where id" + "'" + id + "',pass='" + pass + "'";
+
+        String sql = "select * from Diary where id='" + id + "'and pass='" + pass + "'";
         Cursor cursor = sqlitedb.rawQuery(sql, null);
-        if (cursor.moveToNext()) {
-            Intent it = new Intent(this, Main.class);
-            it.putExtra("it_id", id);
+        if(cursor.moveToNext()){
+
+            Intent it = new Intent(this,Main.class);
+            it.putExtra("it_id",id);
             startActivity(it);
             finish();
-        } else
-            Toast.makeText(this, "아이디,비밀번호를 확인해주세요", Toast.LENGTH_LONG).show();
+        }else
+            Toast.makeText(this, "check", Toast.LENGTH_LONG).show();
+
+        cursor.close();
+        sqlitedb.close();
+        dbmanager.close();
     }
+    public void join(View v){
+        dbmanager = new DBmanager(this);
+        sqlitedb = dbmanager.getReadableDatabase();
 
-    public void join(View v) {
-
-
-        EditText et_id = (EditText) findViewById(R.id.id);
-        EditText et_pass = (EditText) findViewById(R.id.pass);
+        EditText et_id = (EditText)findViewById(R.id.id);
+        EditText et_pass = (EditText)findViewById(R.id.pass);
         String str_id = et_id.getText().toString();
         String str_pass = et_pass.getText().toString();
 
-
         String sql = "select * from Diary where id ='" + str_id + "'";
-        if (str_id == sql) {
-            Toast.makeText(this, "중복된 아이디입니다.", Toast.LENGTH_LONG).show();
-        } else {
-            Intent it = new Intent(this, JoinView.class);
-            it.putExtra("it_id", str_id);
-            it.putExtra("it_pass", str_pass);
+        Cursor cursor = sqlitedb.rawQuery(sql, null);
+        if(cursor.moveToNext()){
+            Toast.makeText(this, "jjon", Toast.LENGTH_LONG).show();
+
+        }else{
+            Intent it = new Intent(this,JoinView.class);
+            sql = "insert into Diary (id , pass ) values('" + str_id + "','" + str_pass + "')";
+            sqlitedb.execSQL(sql);
+            it.putExtra("it_id",str_id);
+            it.putExtra("it_pass",str_pass);
             startActivity(it);
             finish();
+            //아이디 두번 입력되면 에러
         }
-    }
 
+        cursor.close();
+        sqlitedb.close();
+        dbmanager.close();
+    }
 }
 
